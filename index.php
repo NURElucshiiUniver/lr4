373b080aebac9613c6c3be35781b2ae62f9290f8
NURElucshiiUniver <grindmachine755@gmail.com>
2023-12-23 01:24:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ЛР №4</title>
+</head>
+<body>
+
+<?php
+
+function readTextFile($filename) { // Функція пошуку та зчитування файлу з кодом
+    if (file_exists($filename)) {
+        return file_get_contents($filename);
+    } else {
+        return "Файл не знайдено.";
+    }
+}
+
+
+function writeTextFile($filename, $content) { // Функція вписування змін в файл
+    file_put_contents($filename, $content);
+}
+?>
+
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $filename = $_POST['filename'] . '.txt'; 
+    $content = $_POST['content']; 
+
+    writeTextFile($filename, $content);
+}
+
+$files = glob("code.txt"); 
+foreach ($files as $file) { // Генерація веб-сторінки на основі коду в файлі
+    $filename = pathinfo($file, PATHINFO_FILENAME); 
+    $content = readTextFile($file); 
+
+    echo "<p>$content</p>";
+
+    echo "<form action='#' method='post'>";
+    echo "<input type='hidden' name='filename' value='$filename'>";
+    echo "<textarea name='content' rows='25' cols='60'>$content</textarea><br>";
+    echo "<input type='submit' value='submit'>";
+    echo "</form>";
+}
+?>
+
+</body>
+</html>
